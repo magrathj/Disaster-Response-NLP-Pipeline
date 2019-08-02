7be90f3293fadd8ab58ec213b2f6d04041bdca89
magrathj <magrathj@tcd.ie>
2019-08-02 18:16:58 +0100

    trying to fix app

--- disaster_app/run.py
+++ disaster_app/run.py
@@ -21,73 +21,11 @@ from sqlalchemy.engine import reflection
 
 
 
-
-# load data
-print("*******************************")
-engine = create_engine('sqlite:///disaster_app/DisasterResponse.db')
-insp = reflection.Inspector.from_engine(engine)
-print(insp.get_table_names())
-print("********engine************")
-df = pd.read_sql_table('DisasterMessages', engine)
-print("*****************************")
-# load model
-model = joblib.load("disaster_app/classifier.pkl")
-print("model")
-
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
-    
-    # encode plotly graphs in JSON
-    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+   
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('master.html')
 
-
-# web page that handles user query and displays model results
-@app.route('/go', methods=['POST', 'GET'])
-def go():
-    # save user input in query
-    query = request.args.get('query', '') 
-
-    # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
-
-    # This will render the go.html Please see that file. 
-    return render_template(
-        'go.html',
-        query=query,
-        classification_result=classification_results
-    )
