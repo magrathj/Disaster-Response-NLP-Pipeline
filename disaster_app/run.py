d2bb3503fbd57fdf980072f8fb33fdcf627207a4
magrathj <magrathj@tcd.ie>
2019-08-04 12:56:20 +0100

    updated run

--- disaster_app/run.py
+++ disaster_app/run.py
@@ -20,42 +20,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import reflection
 
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('df', engine)
+df = pd.read_sql_table('DisasterMessages', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
 
+print("here")
 
 
 
